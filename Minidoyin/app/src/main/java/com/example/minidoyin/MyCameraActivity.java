9524c7e3c483ae83e1c4bf6cfe0c424cfb69193c
ch1206643805 <1206643805@qq.com>
2019-07-07 23:18:47 +0800

    add camera

--- Minidoyin/app/src/main/java/com/example/minidoyin/MyCameraActivity.java
+++ Minidoyin/app/src/main/java/com/example/minidoyin/MyCameraActivity.java
@@ -1,22 +1,199 @@
 package com.example.minidoyin;
 
+import android.hardware.Camera;
+import android.hardware.camera2.CameraDevice;
+import android.media.CamcorderProfile;
+import android.media.MediaRecorder;
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Surface;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class MyCameraActivity extends AppCompatActivity {
+
+    private static final String TAG = "MyCameraActivity";
+    private int CAMERA_TYPE = Camera.CameraInfo.CAMERA_FACING_BACK;
+    private int TYPE_OUTFILE_IMG = 0;
+    private int TYPE_OUTFILE_VIDEO = 1;
+
+    private int rotationDegree = 0; //旋转
+    private boolean isRecording=false;
+
+    private SurfaceView msurfaceView;
+    private SurfaceHolder msurfaceHolder;
+    private android.hardware.Camera mcamera;
+    private MediaRecorder mmediaRecorder;
+
+
+    private Button button_flip;
+    private Button button_upload;
+    private Button button_exit;
+    private Button button_recording;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_camera);
+        init();
+    }
+
+    /*
+    @effect:初始
+     */
+    public void init(){
+        button_recording = findViewById(R.id.buttonRecording);
+        button_flip = findViewById(R.id.buttonFlip);
+        button_exit = findViewById(R.id.buttonExit);
+        button_upload = findViewById(R.id.buttonUpload);
+
+        mcamera = getCamera(CAMERA_TYPE);
+        msurfaceView = findViewById(R.id.surfaceview);
+        msurfaceHolder = msurfaceView.getHolder();
+        msurfaceHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+        msurfaceHolder.addCallback(new SurfaceHolder.Callback() {
+            @Override
+            public void surfaceCreated(SurfaceHolder holder) {
+                try {
+                    mcamera.setPreviewDisplay(holder);
+                    mcamera.startPreview();
+                    AutoFocus();
+                }catch (Exception e){
+                    e.printStackTrace();
+                }
+            }
+            @Override
+            public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) { }
+            @Override
+            public void surfaceDestroyed(SurfaceHolder holder) {
+                mcamera.stopPreview();
+                mcamera.release();
+                mcamera = null;
+            }
+        });
+
+        //设置视频录制
+        button_recording.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(isRecording){
+                    //结束录制
+                    mmediaRecorder.stop();
+                    mmediaRecorder.reset();
+                    mmediaRecorder.release();
+                    mmediaRecorder=null;
+                    mcamera.lock();
+                    isRecording=false;
+                    Toast.makeText(MyCameraActivity.this,"结束录制",Toast.LENGTH_LONG).show();
+                }else{
+                    //开始录制
+                    Toast.makeText(MyCameraActivity.this,"开始录制",Toast.LENGTH_LONG).show();
+                    isRecording = true;
+                    mmediaRecorder = new MediaRecorder();
+                    mcamera.unlock();
+                    mmediaRecorder.setCamera(mcamera);
+                    mmediaRecorder.setAudioSource(MediaRecorder.AudioSource.CAMCORDER);
+                    mmediaRecorder.setVideoSource(MediaRecorder.VideoSource.CAMERA);
+                    mmediaRecorder.setProfile(CamcorderProfile.get(CamcorderProfile.QUALITY_HIGH));
+
+                    mmediaRecorder.setOutputFile(getOutputFile(TYPE_OUTFILE_VIDEO).toString());
+                    mmediaRecorder.setPreviewDisplay(msurfaceView.getHolder().getSurface());
+                    mmediaRecorder.setOrientationHint(rotationDegree);
+
+                    try {
+                        mmediaRecorder.prepare();
+                        mmediaRecorder.start();
+                    }catch (Exception e){
+                        //释放 mmediaRecorder
+                        mmediaRecorder.stop();
+                        mmediaRecorder.reset();
+                        mmediaRecorder.release();
+                        mmediaRecorder=null;
+                        e.printStackTrace();
+                    }
+                }
+            }
+        });
+        ///
+
+
     }
 
     /*
     @effect:打开摄像头
      */
-    private  void openCamera(){
+    private  Camera getCamera(int position){
+        CAMERA_TYPE = position;
+        if (mcamera != null) {
+            mcamera.release();
+        }
+        Camera cam = Camera.open(position);
+        rotationDegree =getCameraDisplayOrientation(position);
+        cam.setDisplayOrientation(rotationDegree);
 
+        return cam;
+    }
 
+    /*
+    @effect:旋转手机屏幕
+     */
+    private int getCameraDisplayOrientation(int cameraId){
+        Camera.CameraInfo info = new Camera.CameraInfo();
+        int rotation = getWindowManager().getDefaultDisplay().getRotation();
+        int degrees = 0;
+        switch (rotation) {
+            case Surface.ROTATION_0: {
+                degrees = 0; //DEGREE_0
+                break;
+            }
+            case Surface.ROTATION_90:{
+                degrees = 90 ; //DEGREE_90
+                break;
+            }
+            case Surface.ROTATION_180:{
+                degrees = 180; //DEGREE_180
+                break;
+            }
+            case Surface.ROTATION_270:{
+                degrees = 270; //DEGREE_270
+                break;
+            }
+            default:
+                break;
+        }
+        int result;
+        if(info.facing ==Camera.CameraInfo.CAMERA_FACING_FRONT){
+            result = (info.orientation + degrees)%360;
+            result = (360 - result)%360;
+        }else{
+            result =(info.orientation -degrees +360)%360;
+        }
+        return result;
+    }
+
+    /*
+    @effect:后置摄像头 自动调焦
+     */
+    private void AutoFocus(){
+        mcamera.autoFocus(new Camera.AutoFocusCallback() {
+            @Override
+            public void onAutoFocus(boolean success, Camera camera) {
+                if(success){
+                    mcamera.cancelAutoFocus();
+                    Camera.Parameters parameters = camera.getParameters();
+                    parameters.setFocusMode(Camera.Parameters.FOCUS_MODE_CONTINUOUS_VIDEO);
+                    camera.setParameters(parameters);
+                }
+            }
+        });
     }
 
     /*
@@ -27,6 +204,33 @@ public class MyCameraActivity extends AppCompatActivity {
 
     }
 
+    /*
+    @effect:获取输出File，设置其存储路径
+     */
+    private File getOutputFile(int type){
+        File outputFileDir = new File(Environment.getExternalStoragePublicDirectory(
+                Environment.DIRECTORY_DCIM),"MiniDoYin");
+        if(!outputFileDir.exists()){
+            if(!outputFileDir.mkdir()){
+                return null;
+            }
+        }
 
+        String timeStamp=new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        File outputFile;
+        if(type==TYPE_OUTFILE_IMG){
+            //图片
+            outputFile = new File(outputFileDir.getPath()+File.separator+
+                    "IMG_"+timeStamp+".jpg");
+        }else if(type == TYPE_OUTFILE_VIDEO){
+            //视频
+            outputFile = new File(outputFileDir.getPath() + File.separator+
+                    "VID_"+timeStamp+".mp4");
+        }else{
+            return  null;
+        }
+
+        return outputFile;
+    }
 
 }
--- Minidoyin/app/src/main/java/com/example/minidoyin/Page_Activity.java
+++ Minidoyin/app/src/main/java/com/example/minidoyin/Page_Activity.java
@@ -1,12 +1,21 @@
 package com.example.minidoyin;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.content.ContextCompat;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.webkit.WebView;
+import android.widget.Button;
+import android.widget.Toast;
 
 import com.example.minidoyin.Fragment.follow_page_Fragment;
 import com.example.minidoyin.Fragment.home_page_Fragment;
@@ -15,13 +24,20 @@ import com.example.minidoyin.Fragment.message_page_Fragment;
 
 public class Page_Activity extends AppCompatActivity {
     private static final String TAG = "Page_Activity";
-
+    private Button button_camera;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_page_);
-
-        init_viewpage();
+        button_camera = findViewById(R.id.camera_button);
+        button_camera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Page_Activity.this,MyCameraActivity.class);
+                startActivity(intent);
+            }
+        });
+        requestPremission();
     }
 
 
@@ -29,6 +45,7 @@ public class Page_Activity extends AppCompatActivity {
     @effect: 在viewpage根据页数初始化不同的fragment
      */
     private void init_viewpage(){
+
         ViewPager pager = findViewById(R.id.view_page);
         pager.setAdapter(new FragmentPagerAdapter(getSupportFragmentManager()) {
             @Override
@@ -67,15 +84,29 @@ public class Page_Activity extends AppCompatActivity {
 
 
 
-
-
-
-
-
     /*
     @effect: 申请权限
      */
     private void requestPremission(){
+        //申请相机、麦克风、存储的权限
+        if((ContextCompat.checkSelfPermission(this,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE)!= PackageManager.PERMISSION_GRANTED)||
+                ContextCompat.checkSelfPermission(this,
+                        Manifest.permission.CAMERA)!=PackageManager.PERMISSION_GRANTED||
+                ContextCompat.checkSelfPermission(this,
+                        Manifest.permission.RECORD_AUDIO)!=PackageManager.PERMISSION_GRANTED||
+                ContextCompat.checkSelfPermission(this,
+                        Manifest.permission.INTERNET)!=PackageManager.PERMISSION_GRANTED||
+                ContextCompat.checkSelfPermission(this,
+                        Manifest.permission.ACCESS_NETWORK_STATE)!=PackageManager.PERMISSION_GRANTED
+
+        ){
+            ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                    Manifest.permission.CAMERA,Manifest.permission.RECORD_AUDIO,Manifest.permission.INTERNET,Manifest.permission.ACCESS_NETWORK_STATE},111);
+        }
+        else {
+            init_viewpage();
+        }
 
     }
 
@@ -85,6 +116,24 @@ public class Page_Activity extends AppCompatActivity {
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode){
+            case 111:{
+                boolean permission_flag = true;
+                for (int i=0;i<grantResults.length;i++){
+                    if(grantResults[i]!=PackageManager.PERMISSION_GRANTED){
+                        permission_flag=false;
+                    }
+                }
+                if(permission_flag){
+                    init_viewpage();
+                }
+                else {
+                    Toast.makeText(this,"没有权限",Toast.LENGTH_LONG).show();
+                }
+
+                break;
+            }
+        }
     }
 
 
